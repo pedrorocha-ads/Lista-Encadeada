f4a785ee7f1ec7e6add5ee1aac9fbeab8188d6fe
Pedro Rocha <161658584+pedrorocha-ads@users.noreply.github.com>
2025-03-10 20:01:33 -0300

    Duplamente encadeada

--- LIstaEncadeada/src/Main.java
+++ LIstaEncadeada/src/Main.java
@@ -1,23 +1,90 @@
 public class Main {
     public static void main(String[] args) {
-        ListaEncadeada lista = new ListaEncadeada();
-        lista.insereFinal(10);
-        lista.insereFinal(20);
-        lista.insereFinal(30);
-        lista.insereInicio(5);
-        lista.insereInicio(2);
-        lista.imprime();
+
+
     }
 }
 
 class Nodo {
     int elemento;
     Nodo proximo;
+    Nodo anterior;
 
     public Nodo(int elemento) { // Construtor
         this.elemento = elemento;
         this.proximo = null;
+        this.anterior = null;
     }
+}
+
+class DuplamenteEncadeada{
+    private Nodo inicio;
+    private Nodo ultimo;
+    private int n_elementos;
+
+    public DuplamenteEncadeada(){
+        this.inicio = null;
+        this.ultimo = null;
+        this.n_elementos = 0;
+    }
+    public boolean estaVazia(){
+        return this.n_elementos == 0;
+    }
+    public void insereFinal(int elemento){
+        Nodo novoNodo = new Nodo(elemento);
+        if(estaVazia()){
+            inicio = novoNodo;
+            ultimo = novoNodo;
+        }else{
+            ultimo.proximo = novoNodo;
+            novoNodo.anterior = ultimo;
+            ultimo = novoNodo;
+        }
+        n_elementos++;
+    }
+    public void insereInicio(int n_elementos){
+        Nodo novoNodo = new Nodo(n_elementos);
+        if(estaVazia()){
+            inicio = novoNodo;
+            ultimo = novoNodo;
+        }else{
+            novoNodo.proximo = inicio;
+            inicio.anterior = novoNodo;
+            inicio = novoNodo;
+        }
+        n_elementos++;
+    }
+    public void inserePosicao(int elemento, int posicao){
+        if(posicao < 0 || posicao > n_elementos){
+            System.out.println("Posição inválida");
+        }else if(posicao == 0){
+            insereInicio(elemento);
+        }else if(posicao == n_elementos){
+            insereFinal(elemento);
+        }else{
+            Nodo novoNodo = new Nodo(elemento);
+            Nodo atual = inicio;
+            for(int i = 0; i < posicao - 1; i++){
+                atual = atual.proximo;
+            }
+            novoNodo.proximo = atual.proximo;
+            novoNodo.anterior = atual;
+            atual.proximo = novoNodo;
+            n_elementos++;
+        }
+    }
+    public void imprime(){
+        if(estaVazia()){
+            System.out.println("Lista vazia");
+        }else{
+            System.out.print("Lista: ");
+            Nodo atual = inicio;
+            while(atual != null){
+                System.out.print(atual.elemento + " ");
+                atual = atual.proximo;
+            }
+            System.out.println();
+        }
 }
 
 class ListaEncadeada { // Lista Encadeada
@@ -89,4 +156,5 @@ class ListaEncadeada { // Lista Encadeada
             System.out.println();
         }
     }
+}
 }
